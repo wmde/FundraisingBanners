ea2c02a7abce9cf3ed6580ee9515c86bc864fcc9
Leszek Manicki <leszek.manicki@wikimedia.de>
2015-10-05 12:04:06 +0200

    Wrap Qunit tests in IIFE, and put test cases into QUnit's modules

--- tests/banner.tests.js
+++ tests/banner.tests.js
@@ -4,8 +4,11 @@
  * @licence GNU GPL v2+
  * @author Kai Nissen <kai.nissen@wikimedia.de>
  */
-Banner.config.setConfig(
-	{
+( function( Banner, QUnit ) {
+
+	QUnit.module( 'Banner.tracking' );
+
+	Banner.config.setConfig( {
 		encryption: {
 			libUrl: '../js/lib/openpgp.min.js',
 			publicKey:
@@ -53,39 +56,42 @@ Banner.config.setConfig(
 				}
 			}
 		}
-	}
-);
-
-QUnit.test( 'setConfig() overrides configuration properly', function ( assert ) {
-	assert.equal(
-		Banner.config.tracking.events.BANNER_CLOSED.sample,
-		0.8,
-		'configuration overwritten'
-	);
-
-	assert.equal(
-		Banner.config.tracking.campaign,
-		'some-campaign-name',
-		'configuration overwritten'
-	);
-} );
-
-QUnit.test( 'Banner.tracking.shouldTrack() returns correct values', function ( assert ) {
-	assert.equal( Banner.tracking.shouldTrack( 'BANNER_CLOSED', 0.05 ), true );
-	assert.equal( Banner.tracking.shouldTrack( 'BANNER_CLOSED', 0.85 ), false );
-	assert.equal( Banner.tracking.shouldTrack( 'BANNER_EXPANDED', 0.05 ), false );
-} );
-
-QUnit.test( 'Click handlers are set', function( assert ) {
-	assert.ok( $._data( $( '#qunit-fixture' ).get( 0 ), 'events' ) );
-	assert.notOk( $._data( $( '#qunit' ).get( 0 ), 'events' ) );
-} );
-
-QUnit.test( 'Message can be encrypted', function( assert ) {
-	var done = assert.async();
-
-	Banner.encryption.encrypt( 'Hello, Dexter Morgan!' ).then( function( pgpMessage ) {
-		assert.ok( pgpMessage.indexOf( 'BEGIN PGP MESSAGE' ) !== -1 );
-		done();
 	} );
-} );
+
+	QUnit.test( 'setConfig() overrides configuration properly', function( assert ) {
+		assert.equal(
+			Banner.config.tracking.events.BANNER_CLOSED.sample,
+			0.8,
+			'configuration overwritten'
+		);
+
+		assert.equal(
+			Banner.config.tracking.campaign,
+			'some-campaign-name',
+			'configuration overwritten'
+		);
+	} );
+
+	QUnit.test( 'Banner.tracking.shouldTrack() returns correct values', function( assert ) {
+		assert.equal( Banner.tracking.shouldTrack( 'BANNER_CLOSED', 0.05 ), true );
+		assert.equal( Banner.tracking.shouldTrack( 'BANNER_CLOSED', 0.85 ), false );
+		assert.equal( Banner.tracking.shouldTrack( 'BANNER_EXPANDED', 0.05 ), false );
+	} );
+
+	QUnit.test( 'Click handlers are set', function( assert ) {
+		assert.ok( $._data( $( '#qunit-fixture' ).get( 0 ), 'events' ) );
+		assert.notOk( $._data( $( '#qunit' ).get( 0 ), 'events' ) );
+	} );
+
+	QUnit.module( 'Banner.encryption' );
+
+	QUnit.test( 'Message can be encrypted', function( assert ) {
+		var done = assert.async();
+
+		Banner.encryption.encrypt( 'Hello, Dexter Morgan!' ).then( function( pgpMessage ) {
+			assert.ok( pgpMessage.indexOf( 'BEGIN PGP MESSAGE' ) !== -1 );
+			done();
+		} );
+	} );
+
+}( Banner, QUnit ) );
