65810f3ef6500543d3279e4a003adf0b203ea0ac
Gabriel Birke <gabriel.birke@wikimedia.de>
2015-10-15 12:12:17 +0200

    Move Banner js configuration to its own file.

--- sensitive-banner-static/sensitive-banner.inc.php
+++ sensitive-banner-static/sensitive-banner.inc.php
@@ -14,55 +14,7 @@
 <script type="text/javascript" src="../js/banner/banner.encryption.js"></script>
 <script type="text/javascript" src="../js/banner/banner.api.js"></script>
 <script type="text/javascript" src="../js/banner/banner.form.js"></script>
-<script type="text/javascript">
-	// TODO: Move configuration to external file so each dev can test this page
-	Banner.config.setConfig(
-		{
-			api: {
-				apiUrl: 'http://spenden.wikimedia.dev/ajax.php'
-			},
-			encryption: {
-				publicKey:
-				'-----BEGIN PGP PUBLIC KEY BLOCK-----\n' +
-				'Version: GnuPG v1\n' +
-				'\n' +
-				'mQENBFYc2U4BCADHHmqPWBvZgu4L5fS3cub/zhQvdhMwYLEpw6d+hCpZ+tRGBPPN\n' +
-				'oaAEGyBGCgQhCBoGZ7GMRRWHQVTcT55EVTyuWmA3HlnllOfgi28OE4K5g5ekGs4f\n' +
-				'wZ6C0zIbzVrdNMLYXc1/2oTLDXzoAlerdJzY/qM1rYsyzeYWTtPBLzV1R78Z30uT\n' +
-				'+9QokLz5+ryhlFiZHip1ZJzm9i1JM6r/QCppiLv4vbeOMgCHYmBo5+rteY4CR+qB\n' +
-				'bzOGRhOqMIeWvRToECDh7KVDEE/fCuR9QshBmDdiJ8/yK8XuI1ktjCs01/yq+BGV\n' +
-				'7ZK4DF2yb/1keeq/z9CkhN2egM5RKu91PgiLABEBAAG0KVdXVyBEYXRhIDxpbmZv\n' +
-				'QGZ1bmRyYWlzaW5nLndpa2ltZWRpYS5kZXY+iQE4BBMBAgAiBQJWHNlOAhsDBgsJ\n' +
-				'CAcDAgYVCAIJCgsEFgIDAQIeAQIXgAAKCRC0vhCtrPLoeky0CACHbId6R92u4nk0\n' +
-				'3Sa036r0F3CZy7y6WQpKOQgbk9xSJBDm4DPfaNWoncYv+zz5UTYUlW1ROHqyP1i+\n' +
-				'iAZkEd7kKk1Jx2gRtfuWgRsx3SZ0Y27WQnUuzgTMUGwJZA95hN7OWFHQ2uaeCwaQ\n' +
-				'0cIwDZbpirnKltp8250pFvBjNx5+JTJXw6sYejYVw+oc3wqcdkQJwnSVnc0ZUpqo\n' +
-				'gkCmtLVb9cdtQWlzw/tYo9+4IJfb42SeVZc0PlPONdwHNr5A+54WNGe7z1zGNi4n\n' +
-				'+YfM/Fhu5sdZMPMevr80PO8uUTRtY+oiokRNOe9L447gSuXWffyYqwHSMmLmCyvq\n' +
-				'xcK9mT2nuQENBFYc2U4BCADyup7MMTB16TfMQlMc/nMz1eR/GTgJa31d3Hx9kRj6\n' +
-				'YQ/7HL0O83x74M5CjroqblSUc6pfhncAtOMOffiExF2wJ7z4VujJwAqGnMGJY5rt\n' +
-				'QLjny88cktGbDnu2Frt2zwOrcCzeusEiE/C/fNtX0RXT++M+6CwOVplW7HgkwOxh\n' +
-				'04hAGfvcOBkx7fREX3lIrUW59UHbkmlOTu+z6D03sig0PIXemSYPy1n2DIONzdr1\n' +
-				'cj5IpFd8flYSgR+juHXGoOF9FNX2f+BYh9qoFsuQX5jWspsmNKPen9rwiYWu8tSK\n' +
-				'63JaVzhu/3J/T0eQiZ9Tg1uxztae3zIrAcskFaCLseeFABEBAAGJAR8EGAECAAkF\n' +
-				'AlYc2U4CGwwACgkQtL4Qrazy6Hr1PwgAioVRpYxzL0xPwlofS2wLTVsBwbiiO+rG\n' +
-				'UDMQZrbBv6COd4U3amWx5DRgkArdkTmpRkVrJ02mfAVz+dtKqLI/TtmjiTct0gbH\n' +
-				'aq59/VcXqtF8hnil3V+8G8l8HGHb421mt1A1CsMXPNixVAWKGuLZys3uhzbNbtCg\n' +
-				'K0FQc1d70xzCJ4XPTmEucPo5jO0K0OJSXfPsN3+YZAvm/a39GtqeL9zgWqPkdBRn\n' +
-				'h0kZ/ThnqE6kjCPmrEg8bvXNuWr/hRxFqyo5lFdlecCmbdqP+JUUUWy6ehDFiJiW\n' +
-				'PNyEy4s00sBi0sNegG+kiUZRDwuCjvAbdpl9GMgDiCttJ6eeMl3Gbg==\n' +
-				'=zvc7\n' +
-				'-----END PGP PUBLIC KEY BLOCK-----\n'
-			},
-			form: {
-				formId: 'donationForm',
-				formAction: 'http://spenden.wikimedia.dev/spenden/index.php'
-			}
-		}
-	);
-</script>
-
-
+<?php include __DIR__ . '/sensitive-banner-js-config.inc.php'; ?>
 
 <div style="top: 0; display: block;" id="WMDE_Banner">
 
